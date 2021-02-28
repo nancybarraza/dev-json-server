6aba97d92c42c14ff4e2bdecf2ed476a26692088
C Abraham Garcia <garccom@gmail.com>
2021-02-28 07:45:45 -0600

    Missing Json files data

---
+++ db/sites-service/site-events.js
@@ -0,0 +1,98 @@
+module.exports = [
+  {
+    name: "Warning",
+    events: [
+      {
+        siteId: 1,
+        deviceGroupId: 1,
+        deviceId: 1,
+        deviceName: "BATT1",
+        title: "Warning BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+
+      {
+        siteId: 1,
+        deviceGroupId: 7,
+        deviceId: 5,
+        deviceName: "BATT1",
+        title: "Warning BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+      {
+        siteId: 1,
+        deviceGroupId: 11,
+        deviceId: 6,
+        deviceName: "BATT1",
+        title: "Warning BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+    ],
+  },
+  {
+    name: "Fault",
+    events: [
+      {
+        siteId: 1,
+        deviceGroupId: 11,
+        deviceId: 2,
+        deviceName: "BATT1",
+        title: "Fault BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+      {
+        siteId: 1,
+        deviceGroupId: 5,
+        deviceId: 4,
+        deviceName: "BATT1",
+        title: "Fault BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+      {
+        siteId: 1,
+        deviceGroupId: 9,
+        deviceId: 8,
+        deviceName: "BATT1",
+        title: "Fault BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+    ],
+  },
+  {
+    name: "Alarm",
+    events: [
+      {
+        siteId: 1,
+        deviceGroupId: 4,
+        deviceId: 3,
+        deviceName: "BATT1",
+        title: "Alarm BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+      {
+        siteId: 1,
+        deviceGroupId: 9,
+        deviceId: 7,
+        deviceName: "BATT1",
+        title: "Alarm BATT1",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys.",
+        eventDateTime: "2020-12-03T12:54:28.555391",
+      },
+    ],
+  },
+];
