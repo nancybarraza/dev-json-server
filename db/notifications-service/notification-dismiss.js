478a56110a958d4387f1a619d537debfcaad2c8e
Luis Medina <lmedina@anexinet.com>
2021-03-01 11:41:48 -0600

    Add mock data for sites and notifications

---
+++ db/notifications-service/notification-dismiss.js
@@ -0,0 +1,7 @@
+module.exports = {
+  id: "602403d4973bb702f165485a",
+  title: "Notification Title",
+  text: "Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline.",
+  siteId: 1,
+  ts: "2021-01-10 10:03:24",
+};
---
+++ db/notifications-service/notifications.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: "602403d4973bb702f165485a",
+    title: "Notification Title",
+    text:
+      "Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline.",
+    siteId: 1,
+    ts: "2021-01-10 10:03:24",
+  },
+  {
+    id: "602403d4973bb702f365845a",
+    title: "Notification Title 2",
+    text:
+      "Schneider Electric BB-466-02 Serial Number 321648FBD-FOO is Offline.",
+    siteId: 9,
+    ts: "2021-02-09 10:03:24",
+  },
+  {
+    id: "602403d4973bb702f354545a",
+    title: "Notification Title 3",
+    text:
+      "Schneider Electric BB-466-02 Serial Number 569872FBD-FOO is Offline.",
+    siteId: 3,
+    ts: "2021-01-18 10:03:24",
+  },
+  {
+    id: "602403d4973bb702f125385a",
+    title: "Notification Title 4",
+    text:
+      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline.",
+    siteId: 5,
+    ts: "2021-02-08 13:03:24",
+  },
+];
