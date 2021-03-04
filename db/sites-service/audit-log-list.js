7c4372b1f4446ac45199e771b3e8715beddd0a97
Nancy <nbarraza@anexinet.com>
2021-03-04 13:25:22 -0600

    Adding audit log json data updated

--- db/sites-service/audit-log-list.js
+++ db/sites-service/audit-log-list.js
@@ -1,67 +1,67 @@
 module.exports = [
   {
     id: 1,
-    date: "Sep 18, 2020, 2:59:31 PM",
-    title: "Change in Bus Band Configuration",
-    description:
-      '"The Bus Band parameter"  Bus Band Parameter Name  "was changed to"  New Value. The prior value was" Old Value "."',
-    author: "Changed By: David Butler",
+    actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885453427"
   },
   {
     id: 2,
-    date: "Sep 19, 2020, 2:59:31 PM",
-    title: "New Device detected at Site",
-    description:
-      '"A new" Device Type Make Model "Serial Number" Serial Number "was added to the Site." ',
-    author: "DCentrIQ",
+   actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885453427"
   },
   {
     id: 3,
-    date: "Sep 20, 2020, 2:59:31 PM",
-    title: 'Site Name  "has been set to Locked Mode"',
-    description: '"The site is currently in locked status."',
-    author: "Changed By: David Joe",
+     actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885453427"
   },
   {
     id: 4,
     date: "Jan 1, 2020, 2:59:31 PM",
-    title: 'Site Name  "has been set to Locked Mode"',
-    description: '"The site is currently in locked status."',
-    author: "Changed By: David Joe",
+     actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885453427"
   },
   {
     id: 5,
     date: "Jan 2, 2020, 2:59:31 PM",
-    title: 'Site Name  "has been set to Locked Mode"',
-    description: '"The site is currently in locked status."',
-    author: "Changed By: David Joe",
+    actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885734119"
   },
   {
     id: 6,
-    date: "Jan 3, 2020, 2:59:31 PM",
-    title: 'Site Name  "has been set to Locked Mode"',
-    description: '"The site is currently in locked status."',
-    author: "Changed By: David Joe",
+    actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885740095"
   },
   {
     id: 7,
-    date: "Jan 4, 2020, 2:59:31 PM",
-    title: 'Site Name  "has been set to Locked Mode"',
-    description: '"The site is currently in locked status."',
-    author: "Changed By: David Joe",
+     actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885744807"
   },
   {
     id: 8,
-    date: "Jan 5, 2020, 2:59:31 PM",
-    title: 'Site Name  "has been set to Locked Mode"',
-    description: '"The site is currently in locked status."',
-    author: undefined,
+   actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885750602"
   },
   {
     id: 9,
-    date: "Jan 6, 2020, 2:59:31 PM",
-    title: 'Site Name  "has been set to Locked Mode"',
-    description: '"The site is currently in locked status."',
-    author: null,
+    actionType : "Change in Bus Band Configuration",
+    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
+    source : "David Butler",
+    date : "1614885864283"
   },
 ];
