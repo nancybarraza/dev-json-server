809318d28568a6fccf952bffc898e065dd11bec0
jayayyap <80050037+jayayyap@users.noreply.github.com>
2021-03-19 17:26:26 -0400

    Update notifications.js
    added initial JA to text

--- db/notifications-service/notifications.js
+++ db/notifications-service/notifications.js
@@ -3,7 +3,7 @@ module.exports = [
     id: "602403d4973bb702f165485a",
     title: "Notification Title JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline. JA",
+      "JA Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline.",
     siteId: 1,
     ts: "2021-01-10 10:03:24",
   },
@@ -11,7 +11,7 @@ module.exports = [
     id: "602403d4973bb702f365845a",
     title: "Notification Title 2 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 321648FBD-FOO is Offline. JA",
+      "JA Schneider Electric BB-466-02 Serial Number 321648FBD-FOO is Offline.",
     siteId: 9,
     ts: "2021-02-09 10:03:24",
   },
@@ -19,7 +19,7 @@ module.exports = [
     id: "602403d4973bb702f354545a",
     title: "Notification Title 3 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 569872FBD-FOO is Offline. JA",
+      "JA Schneider Electric BB-466-02 Serial Number 569872FBD-FOO is Offline.",
     siteId: 3,
     ts: "2021-01-18 10:03:24",
   },
@@ -27,7 +27,7 @@ module.exports = [
     id: "602403d4973bb702f125385a",
     title: "Notification Title 4 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline. JA",
+      "JA Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline.",
     siteId: 5,
     ts: "2021-02-08 13:03:24",
   },
@@ -35,7 +35,7 @@ module.exports = [
     id: "602403d4973bb702f125385a",
     title: "Notification Title 5 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline yolo. JA",
+      "JA Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline yolo.",
     siteId: 5,
     ts: "2021-02-08 13:03:24",
   },
