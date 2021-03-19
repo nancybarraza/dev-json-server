012e763c51226844267ee9c15f71d4f0aebf80c5
jayayyap <80050037+jayayyap@users.noreply.github.com>
2021-03-19 17:49:25 -0400

    Update notifications.js
    Removed Initials JA from Title and Text

--- db/notifications-service/notifications.js
+++ db/notifications-service/notifications.js
@@ -1,41 +1,41 @@
 module.exports = [
   {
     id: "602403d4973bb702f165485a",
-    title: "Notification Title JA",
+    title: "Notification Title",
     text:
-      "JA Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline.",
     siteId: 1,
     ts: "2021-01-10 10:03:24",
   },
   {
     id: "602403d4973bb702f365845a",
-    title: "Notification Title 2 JA",
+    title: "Notification Title 2",
     text:
-      "JA Schneider Electric BB-466-02 Serial Number 321648FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 321648FBD-FOO is Offline.",
     siteId: 9,
     ts: "2021-02-09 10:03:24",
   },
   {
     id: "602403d4973bb702f354545a",
-    title: "Notification Title 3 JA",
+    title: "Notification Title 3",
     text:
-      "JA Schneider Electric BB-466-02 Serial Number 569872FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 569872FBD-FOO is Offline.",
     siteId: 3,
     ts: "2021-01-18 10:03:24",
   },
   {
     id: "602403d4973bb702f125385a",
-    title: "Notification Title 4 JA",
+    title: "Notification Title 4",
     text:
-      "JA Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline.",
     siteId: 5,
     ts: "2021-02-08 13:03:24",
   },
   {
     id: "602403d4973bb702f125385a",
-    title: "Notification Title 5 JA",
+    title: "Notification Title 5",
     text:
-      "JA Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline yolo.",
+      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline yolo.",
     siteId: 5,
     ts: "2021-02-08 13:03:24",
   },
