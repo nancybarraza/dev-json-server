3e257a4ecb1a9e8dd82075997939ad10ae7279ef
jayayyap <80050037+jayayyap@users.noreply.github.com>
2021-03-19 17:06:49 -0400

    Update notifications.js
    added initials JA to Notification title and text

--- db/notifications-service/notifications.js
+++ db/notifications-service/notifications.js
@@ -1,41 +1,41 @@
 module.exports = [
   {
     id: "602403d4973bb702f165485a",
-    title: "Notification Title",
+    title: "Notification Title JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 3216842FBD-FOO is Offline. JA",
     siteId: 1,
     ts: "2021-01-10 10:03:24",
   },
   {
     id: "602403d4973bb702f365845a",
-    title: "Notification Title 2",
+    title: "Notification Title 2 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 321648FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 321648FBD-FOO is Offline. JA",
     siteId: 9,
     ts: "2021-02-09 10:03:24",
   },
   {
     id: "602403d4973bb702f354545a",
-    title: "Notification Title 3",
+    title: "Notification Title 3 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 569872FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 569872FBD-FOO is Offline. JA",
     siteId: 3,
     ts: "2021-01-18 10:03:24",
   },
   {
     id: "602403d4973bb702f125385a",
-    title: "Notification Title 4",
+    title: "Notification Title 4 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline.",
+      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline. JA",
     siteId: 5,
     ts: "2021-02-08 13:03:24",
   },
   {
     id: "602403d4973bb702f125385a",
-    title: "Notification Title 5",
+    title: "Notification Title 5 JA",
     text:
-      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline yolo.",
+      "Schneider Electric BB-466-02 Serial Number 654874FBD-FOO is Offline yolo. JA",
     siteId: 5,
     ts: "2021-02-08 13:03:24",
   },
