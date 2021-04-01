b2cb36db251eff79b32d3cc79e721d324ef3ed88
Carlos Abraham Garcia <cagarcia@anexinet.com>
2021-04-01 11:23:04 -0600

    Telemetry update to overview, SiteList no points

--- endpoints.js
+++ endpoints.js
@@ -8,6 +8,7 @@ const okta_users = require("./db/users-service/users-okta");
 const sitesList = require("./db/sites-service/sitesList");
 const sitesIcianUsers = require("./db/sites-service/assigned-users");
 const auditLogList = require("./db/sites-service/audit-log-list");
+const overview = require("./db/sites-service/overview");
 const siteEvents = require("./db/sites-service/site-events");
 const siteInformation = require("./db/sites-service/site-information");
 const siteBusBand = require("./db/sites-service/bus-band");
@@ -81,8 +82,9 @@ const endpoints = [
     responseData: siteEvents,
   },
   {
-    endpoint: "/sites-service/sites/:siteId/telemetry",
-    responseKey: "sites_telemetry",
+    endpoint: "/sites-service/sites/:siteId/overview",
+    responseKey: "sites_overview",
+    responseData: overview,
   },
   {
     endpoint: "/sites-service/sites/:siteId/audit-log",
