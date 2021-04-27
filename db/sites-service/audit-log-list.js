10f50552adb3d87eb16b29df8c659a6c59716458
Nancy <nbarraza@anexinet.com>
2021-04-27 15:37:34 -0500

    fix audit log

--- db/sites-service/audit-log-list.js
+++ db/sites-service/audit-log-list.js
@@ -1,70 +1,79 @@
 module.exports = {
-    date: 1614885453427
-    page: 1,
-    siteId: 2,
-    results: [
-  {
-    siteId: 1,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885453427"
-  },
-  {
-    siteId: 2,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885453427"
-  },
-  {
-    siteId: 3,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885453427"
-  },
-  {
-    siteId: 4,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885453427"
-  },
-  {
-    siteId: 5,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885734119"
-  },
-  {
-    siteId: 6,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885740095"
-  },
-  {
-    siteId: 7,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885744807"
-  },
-  {
-    siteId: 8,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885750602"
-  },
-  {
-    siteId: 9,
-    actionType : "Change in Bus Band Configuration",
-    actionDescription : "The Bus Band parameter \"Bus Band Parameter Name\" was changed to Busband New Name. The prior value was Busband Old Name",
-    source : "David Butler",
-    date : "1614885864283"
-  },
-]
-}
+	date: 1614885453427,
+	page: 1,
+	siteId: 2,
+	results: [
+		{
+			siteId: 1,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885453427',
+		},
+		{
+			siteId: 2,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885453427',
+		},
+		{
+			siteId: 3,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885453427',
+		},
+		{
+			siteId: 4,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885453427',
+		},
+		{
+			siteId: 5,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885734119',
+		},
+		{
+			siteId: 6,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885740095',
+		},
+		{
+			siteId: 7,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885744807',
+		},
+		{
+			siteId: 8,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885750602',
+		},
+		{
+			siteId: 9,
+			actionType: 'Change in Bus Band Configuration',
+			actionDescription:
+				'The Bus Band parameter "Bus Band Parameter Name" was changed to Busband New Name. The prior value was Busband Old Name',
+			source: 'David Butler',
+			date: '1614885864283',
+		},
+	],
+};
